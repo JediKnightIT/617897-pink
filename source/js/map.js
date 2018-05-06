d01a5c70bb171b3f73663b517b46a5146999b29a
Niyaz <Niayz_Indicus@mail.ru>
2018-05-07 00:00:25 +0300

    Добавляет интерактивную карту с стилизованным маркером

---
+++ source/js/map.js
@@ -0,0 +1,16 @@
+function initMap() {
+  var uluru = {
+    lat: 59.936107,
+    lng: 30.321490
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: uluru
+  });
+  var markerIcon = "../img/icon-map-marker.svg";
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    icon: markerIcon
+  });
+}
