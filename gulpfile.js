711d5369bde42862e1e4b155a8373f4eca2f1899
Niyaz Garipov <Niayz_Indicus@mail.ru>
2018-05-17 22:56:00 +0300

    Добавляет gulp-imagemin и создаёт task для оптимизации изображений

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var imagemin = require("gulp-imagemin");
 var server = require("browser-sync").create();
 
 gulp.task("style", function() {
@@ -16,6 +17,16 @@ gulp.task("style", function() {
     ]))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("serve", ["style"], function() {
